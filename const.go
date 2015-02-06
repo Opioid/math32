459c2f53781bc7ac69333a8b6d1adcb5880823e4
beni <benjamin.savs@gmail.com>
2015-02-06 13:59:53 +0100

    Added constants

---
+++ const.go
@@ -0,0 +1,14 @@
+// Copyright 2014 Benjamin Savs.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package math32
+
+import (
+	"math"
+)
+
+const (
+	Pi = math.Pi
+	InvPi = 1.0 / math.Pi
+)
